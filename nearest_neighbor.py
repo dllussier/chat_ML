d54cc1f1a9061a53d84196d0dc0faca4c278258a
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-07-21 23:42:32 -0400

    Update nearest_neighbor.py

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import shuffle
